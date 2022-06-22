a69ce83263da879dace2de660131026372634e49
Pratyush Jain <pratyush.jain2001@gmail.com>
2022-06-22 05:31:39 +0530

    done

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,10 @@ router.get("/", (req, res) => {
     res.render("pages/index");
 });
 
+router.get("/product/details", (req, res) => {
+    res.render("pages/product_details");
+});
+
 router.use("/supplier", require("./supplier"));
 
 module.exports = router;
