ded05a381af089a00105d6a5da74032ab6c90386
Rahul Mansharamani <rahul2002aug@gmail.com>
2022-06-22 05:31:08 +0530

    register api added

--- src/routes/supplier.js
+++ src/routes/supplier.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const admin = require("../config/db");
+
+const db = admin.firestore();
+const supplierRef = db.collection("Suppliers");
 
 router.get("/register", (req, res) => {
     res.render("pages/supplier_input");
@@ -8,5 +12,34 @@ router.get("/register", (req, res) => {
 router.get("/list", (req, res) => {
     res.render("pages/supplier_list");
 });
+
+router.post("/register", async (req, res) => {
+    const data = req.body.data;
+
+    await supplierRef
+        .doc(data.name)
+        .set(data)
+        .then(() => {
+            console.log("Document successfully written!");
+        })
+        .catch((error) => {
+            console.error("Error writing document: ", error);
+        });
+
+    return data.name;
+});
+
+const vendor_register = async (userid, obj) => {
+    await docRef
+        .doc(userid)
+        .set(obj)
+        .then(() => {
+            console.log("Document successfully written!");
+        })
+        .catch((error) => {
+            console.error("Error writing document: ", error);
+        });
+    return userid;
+};
 
 module.exports = router;
